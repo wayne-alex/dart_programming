b138af061ae9fc6256b1618b19d5985298bf1be3
Wayne Alex <waynealex9914@gmail.com>
2023-02-22 14:48:41 +0300

    fizzBuzz

---
+++ fizzBuzz.dart
@@ -0,0 +1,13 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print(" $i FizzBuzz");
+    } else if (i % 3 == 0) {
+      print("$i Fizz");
+    } else if (i % 5 == 0) {
+      print("$i Buzz");
+    } else {
+      print(i);
+    }
+  }
+}
